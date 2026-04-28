eaee7997bba30a3949bbfd3cda19b5cca8166af8
NirvanaLuo <luohan89@gmail.com>
2026-04-28 11:23:06 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inspector } from 'react-dev-inspector';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -66,6 +67,7 @@ export default function RootLayout({
       <body className={`antialiased`}>
         {isDev && <Inspector />}
         {children}
+        <Analytics />
       </body>
     </html>
   );
